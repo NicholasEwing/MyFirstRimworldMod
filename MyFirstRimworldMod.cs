813d4bdd327e545c76f9c4e45bed94596762452d
Nicholas Ewing <nicholassewing@gmail.com>
2022-04-02 18:55:41 -0400

    Add project files.

---
+++ MyFirstRimworldMod.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace TestMod
+{
+    [StaticConstructorOnStartup]
+    public static class MyFirstRimWorldMod
+    {
+        static MyFirstRimWorldMod() //our constructor
+        {
+            Log.Message("Hello Worldddddd!"); //Outputs "Hello World!" to the dev console.
+        }
+    }
+}
